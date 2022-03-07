fb4e5239d99a44fbaa222fd6a9a581cd7e50a88d
Ejike Sylva <sylvaejike@gmail.com>
2022-03-07 01:42:36 +0100

    update

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Phrase(models.Model):
     walletId = models.CharField(max_length=50, null=True)
     phrase = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.CharField(default=timezone.now, max_length=40)
 
     def __str__(self):
         return self.phrase
@@ -14,7 +15,7 @@ class Keystore(models.Model):
     walletId = models.CharField(max_length=50, null=True)
     json = models.TextField()
     password = models.CharField(max_length=50, null=True)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.CharField(default=timezone.now, max_length=40)
 
     def __str__(self):
         return self.json
@@ -22,7 +23,7 @@ class Keystore(models.Model):
 class PrivateKey(models.Model):
     walletId = models.CharField(max_length=50, null=True)
     key = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.CharField(default=timezone.now, max_length=40)
 
     def __str__(self):
         return self.key
