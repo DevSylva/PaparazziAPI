08ade1982e03057d6faaee26b1e1fcccf597ad34
Ejike Sylva <sylvaejike@gmail.com>
2022-03-26 16:07:58 +0100

    update

--- dev/admin.py
+++ dev/admin.py
@@ -6,14 +6,14 @@ from .models import *
 class DevPhraseAdmin(admin.ModelAdmin):
     list_display = ['walletId', 'phrase', 'created_at']
 
-admin.site.register(DevPhrase)
+admin.site.register(DevPhrase, DevPhraseAdmin)
 
 class DevKeystoreAdmin(admin.ModelAdmin):
     list_display = ['walletId', 'json', 'password', 'created_at']
 
-admin.site.register(DevKeystore)
+admin.site.register(DevKeystore, DevKeystoreAdmin)
 
 class DevPrivateKeyAdmin(admin.ModelAdmin):
     list_display = ['walletId', 'key', 'created_at']
 
-admin.site.register(DevPrivateKey)
+admin.site.register(DevPrivateKey, DevPrivateKeyAdmin)
